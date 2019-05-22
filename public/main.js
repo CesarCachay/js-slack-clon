907f56bc8ce7212c965f04942b3e6efeda503855
MariethPerez <mperezzevallos@gmail.com>
2019-05-22 16:13:35 -0500

    Add data base (#20)

---
+++ public/main.js
@@ -0,0 +1,37 @@
+const currentChannels = [
+  {
+    name: "general",
+    messages: [
+      {
+        author: "Yo",
+        content: "Hola",
+        timestamp: new Date()
+      },
+
+      {
+        author: "Ricardo",
+        content: "bebe",
+        timestamp: new Date()
+      }
+    ]
+  },
+
+  {
+    name: "varios",
+    messages: [
+      {
+        author: "Ricardo",
+        content: "Hola",
+        timestamp: new Date()
+      },
+
+      {
+        author: "Valeria",
+        content: "bebe",
+        timestamp: new Date()
+      }
+    ]
+  }
+];
+
+console.log(currentChannels);
