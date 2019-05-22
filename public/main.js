4d6b900fe7e7f221116e8d8fd031fd2c3e39a8da
Valeria Vassallo <valevassallo1@gmail.com>
2019-05-22 17:26:44 -0500

    Render channels on page (#23)

--- public/main.js
+++ public/main.js
@@ -36,15 +36,26 @@ const currentChannels = [
 
 console.log(currentChannels);
 
-function addChannel(name){
-  const channel =  {
+function addChannel(name) {
+  const channel = {
     name: name,
     messages: []
-  }
-  
-  currentChannels.push(channel)
-}
-  addChannel("love");
-  console.log(currentChannels);
+  };
 
-  
+  currentChannels.push(channel);
+}
+addChannel("love");
+console.log(currentChannels);
+
+const $channels = document.getElementById("channels");
+
+function renderChannel(list) {
+  return list.map(channel => {
+    let p = document.createElement("p");
+
+    p.innerHTML = `<p class="channel-item"># ${channel.name}</p>`;
+    $channels.appendChild(p);
+  });
+}
+
+renderChannel(currentChannels);
