aa331eb06370452f8917320596f407d217d0bbd3
Marieth <mperezzevallos@gmail.com>
2019-05-23 12:48:03 -0500

    Add final changes channel storages

--- public/main.js
+++ public/main.js
@@ -1,34 +1,34 @@
 const currentChannels = [
-  // {
-  //   name: "general",
-  //   messages: [
-  //     {
-  //       author: "Yo",
-  //       content: "Hola",
-  //       timestamp: new Date()
-  //     },
-  //     {
-  //       author: "Ricardo",
-  //       content: "bebe",
-  //       timestamp: new Date()
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "varios",
-  //   messages: [
-  //     {
-  //       author: "Ricardo",
-  //       content: "Hola",
-  //       timestamp: new Date()
-  //     },
-  //     {
-  //       author: "Valeria",
-  //       content: "bebe",
-  //       timestamp: new Date()
-  //     }
-  //   ]
-  // }
+  {
+    name: "general",
+    messages: [
+      {
+        author: "Yo",
+        content: "Hola",
+        timestamp: new Date()
+      },
+      {
+        author: "Ricardo",
+        content: "bebe",
+        timestamp: new Date()
+      }
+    ]
+  },
+  {
+    name: "varios",
+    messages: [
+      {
+        author: "Ricardo",
+        content: "Hola",
+        timestamp: new Date()
+      },
+      {
+        author: "Valeria",
+        content: "bebe",
+        timestamp: new Date()
+      }
+    ]
+  }
 ];
 
 console.log(currentChannels);
@@ -40,22 +40,19 @@ function addChannel(name) {
   };
 
   currentChannels.push(channel);
+  saveChannelStorage(name);
 }
-// addChannel("love");
-// console.log(currentChannels);
 
 const $channels = document.getElementById("channels");
 
 function renderChannel(list) {
   $channels.innerHTML = "";
-  list.map(channel => {
+  return list.map(channel => {
     let p = document.createElement("p");
-    p.innerHTML = `<p class="channel-item" data-name="${channel.name}># ${
-      channel.name
-    }</p>`;
+
+    p.innerHTML = `<p class="channel-item" ># ${channel.name}</p>`;
     $channels.appendChild(p);
   });
-  addListener();
 }
 
 function addChannelListener() {
@@ -103,8 +100,7 @@ function addListener() {
   }
 }
 
-function saveChannelStorage() {
-  const channel = this.dataset.name;
+function saveChannelStorage(channel) {
   localStorage.setItem(channel, channel);
 }
 
