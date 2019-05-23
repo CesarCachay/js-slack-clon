19c4ec330ee54c18aa56c24a4799b331e107f85e
Ricardo Yrupailla Meza <ry.yrupailla@gmail.com>
2019-05-23 15:02:05 -0500

    Add render comments functionality (#31)

--- public/main.js
+++ public/main.js
@@ -68,10 +68,36 @@ const titlePlus = document.getElementById("title-plus");
 titlePlus.addEventListener("click", addChannelListener);
 renderChannel(currentChannels);
 
+function renderComments(channelName) {
+  let channel = currentChannels.find(obj => {
+    return obj.name === channelName;
+  });
+
+  let msgDisplay = document.getElementsByClassName("msg-display")[0];
+  msgDisplay.innerHTML = "";
+  channel.messages.forEach(channelMessage => {
+    const p = document.createElement("p");
+    p.innerHTML = `<p class="message-item">${channelMessage.author} : ${
+      channelMessage.content
+    } ${channelMessage.timestamp}</p>`;
+    msgDisplay.appendChild(p);
+  });
+}
+
+let channelItems = document.getElementsByClassName("channel-item");
+channelItems = Array.from(channelItems);
+
+channelItems.map(channelItem => {
+  const channelName = channelItem.innerText.slice(2);
+  channelItem.addEventListener("click", () => renderComments(channelName));
+});
+
+/// server
+
 const searchUSer = document.getElementById("current_user");
 const savedUser = localStorage.getItem("currentUSer");
 searchUSer.innerText = `${savedUser}`;
-console.log(savedUser);
+// console.log(savedUser);
 
 const socket = new WebSocket("ws://localhost:3000/connection");
 
@@ -93,7 +119,6 @@ function send(msg) {
   );
 }
 
-
 function saveChannelStorage(channel) {
   localStorage.setItem(channel, channel);
 }
@@ -107,7 +132,7 @@ function showChannelStorage() {
 
 showChannelStorage();
 
-function makeComment(){
-  let inputMessage = document.getElementById('input-message').value;
+function makeComment() {
+  let inputMessage = document.getElementById("input-message").value;
   console.log(inputMessage);
 }
