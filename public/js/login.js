92887d8fc14cd3de5e8042b1eeb7337d1b0c2e8b
Valeria Vassallo <valevassallo1@gmail.com>
2019-05-24 12:10:45 -0500

    Add addEventListener & refactor

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,13 @@
 let currentUser = "";
+let form = document.getElementById("login-form");
+
+form.addEventListener("submit", Login);
 
 function Login() {
   var usuario = document.login.usuario.value;
   if (usuario != "") {
     localStorage.setItem("currentUser", usuario);
-    window.location = "main.html";
+    // window.location = "main.html";
   }
   if (usuario == "") {
     alert("You need to write a username!!");
