d97e28fa488beae3e98596108e1ae7fa1607b868
Cesar Vangelis Cachay Arbildo <cesarcachay1996@gmail.com>
2019-05-25 16:45:06 -0500

    Feature/notification new messages (#79)
    * Add notification for new messages
    
    * Fix content notification for new messages
    
    * Add channel name in notification of new message

--- public/js/main.js
+++ public/js/main.js
@@ -246,6 +246,7 @@ socket.addEventListener("close", () => {
 socket.addEventListener("message", event => {
   const newMessages = JSON.parse(event.data);
   receiveComment(newMessages);
+  sendNotification(newMessages);
 });
 
 async function askNotification() {
@@ -256,3 +257,21 @@ async function askNotification() {
 }
 
 askNotification();
+
+function sendNotification(currentUser) {
+  if (
+    currentUser.user != currentUser.name &&
+    currentUser.channel != currentChannels.channel
+  ) {
+    const notification = new Notification(
+      `New message in channel "${currentUser.channel}"`,
+      {
+        body: `${currentUser.user}: ${currentUser.message}`,
+        icon: "./assets/favicon.ico"
+      }
+    );
+    notification.onclick = event => {
+      notification.close();
+    };
+  }
+}
